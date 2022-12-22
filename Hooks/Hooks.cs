5f4aafcd35b1242234c9ad19ef1a1da8d7072552
Jan Tuziak <jan.tuziak@gmail.com>
2022-12-22 13:24:50 +0100

    HTML Reporter added

---
+++ Hooks/Hooks.cs
@@ -0,0 +1,66 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Gherkin.Model;
+using AventStack.ExtentReports.Reporter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecruitmentTaskSpecflowSelenium.Hooks
+{
+    [Binding]
+    internal class Hooks
+    {
+        private static ExtentTest featureName;
+        private static ExtentTest scenario;
+        private static ExtentReports extent;
+
+        [BeforeTestRun]
+        public static void InitializeReport()
+        {
+            string projectDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+
+            var htmlReporter = new ExtentHtmlReporter(Path.Combine(projectDirectory, "OneCrmCloudReport.html"));
+            htmlReporter.Config.Theme = AventStack.ExtentReports.Reporter.Configuration.Theme.Dark;
+            
+            extent = new ExtentReports();
+            extent.AttachReporter(htmlReporter);
+        }
+
+        [AfterTestRun] public static void CleanupReport() { extent.Flush(); }
+        [BeforeFeature] public static void BeforeFeature() { featureName = extent.CreateTest<Feature>(FeatureContext.Current.FeatureInfo.Title); }
+        [BeforeScenario] public static void BeforeScenario() { scenario = featureName.CreateNode<Scenario>(ScenarioContext.Current.ScenarioInfo.Title); }
+        [AfterStep] public static void AfterStep(ScenarioContext sc) 
+        {
+            var stepType = ScenarioStepContext.Current.StepInfo.StepDefinitionType.ToString();
+            PropertyInfo pInfo = typeof(ScenarioContext).GetProperty("ScenarioExecutionStatus", BindingFlags.Instance | BindingFlags.Public);
+            MethodInfo getter = pInfo.GetGetMethod(nonPublic: true);
+            object TestResult = getter.Invoke(sc, null);
+            
+            if (sc.TestError == null)
+            {
+                if (stepType == "Given")
+                    scenario.CreateNode<Given>(ScenarioStepContext.Current.StepInfo.Text);
+                else if (stepType == "When")
+                    scenario.CreateNode<When>(ScenarioStepContext.Current.StepInfo.Text);
+                else if (stepType == "Then")
+                    scenario.CreateNode<Then>(ScenarioStepContext.Current.StepInfo.Text);
+                else if (stepType == "And")
+                    scenario.CreateNode<And>(ScenarioStepContext.Current.StepInfo.Text);
+            }
+            else
+            {
+                if (stepType == "Given")
+                    scenario.CreateNode<Given>(ScenarioStepContext.Current.StepInfo.Text).Fail(sc.TestError.Message);
+                else if (stepType == "When")
+                    scenario.CreateNode<When>(ScenarioStepContext.Current.StepInfo.Text).Fail(sc.TestError.Message);
+                else if (stepType == "Then")
+                    scenario.CreateNode<Then>(ScenarioStepContext.Current.StepInfo.Text).Fail(sc.TestError.Message);
+                else if (stepType == "And")
+                    scenario.CreateNode<And>(ScenarioStepContext.Current.StepInfo.Text).Fail(sc.TestError.Message);
+            }
+        }
+    }
+}
